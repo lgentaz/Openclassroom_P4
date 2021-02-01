7f2f740de9832070c8dad031fe515e5af1c9a0fb
lgentaz <lucile.gentaz@gmail.com>
2021-02-01 18:07:41 +0100

    frequentUser option added to Fare CalculatorService

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,13 +5,15 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
+    // add frequent user variable (::before create/update request for previous ticket in TicketDAO)
+    private boolean frequentUser = false;
+
     /**
      * Calculates the parking fare when the vehicle leaves and saves it in DB
      *
      * @param ticket
      *          the information relative to the parked vehicle
      */
-
     public void calculateFare(Ticket ticket){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
@@ -22,6 +24,8 @@ public class FareCalculatorService {
 
         float duration;
         duration = (float) ((outTime - inTime)/3600.0);
+
+        if(frequentUser) duration *= 0.95;
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
