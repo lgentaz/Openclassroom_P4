d3ff6041ba879ca9691d43286e45dd511211a036
lgentaz <lucile.gentaz@gmail.com>
2021-02-09 16:37:15 +0100

    Correction 30 first minutes free: full price of first 30min when longer stay

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -20,8 +20,8 @@ public class FareCalculatorService {
         long inTime = ticket.getInTime().getTime() / (1000);
         long outTime = ticket.getOutTime().getTime() / (1000);
 
-        double duration = (outTime - inTime)/3600.0 - 0.5;
-        if (duration < 0) duration = 0.0;
+        double duration = (outTime - inTime)/3600.0;
+        if (duration < 0.5) duration = 0.0;
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
