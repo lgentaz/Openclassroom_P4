469af91eba6da6f0a3785bc1907b8fa8e26e07fe
lgentaz <lucile.gentaz@gmail.com>
2021-03-10 11:17:33 +0100

    InputReaderUtil: Catching the correct exception in readVehicleRegistration()

--- src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
+++ src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
  */
 public class InputReaderUtil {
 
-    private static Scanner scan = new Scanner(System.in);
+    private static final Scanner scan = new Scanner(System.in);
     private static final Logger logger = LogManager.getLogger("InputReaderUtil");
 
     /**
@@ -20,8 +20,7 @@ public class InputReaderUtil {
      */
     public int readSelection() {
         try {
-            int input = Integer.parseInt(scan.nextLine());
-            return input;
+            return Integer.parseInt(scan.nextLine());
         }catch(Exception e){
             logger.error("Error while reading user input from Shell", e);
             System.out.println("Error reading input. Please enter valid number for proceeding further");
@@ -35,14 +34,14 @@ public class InputReaderUtil {
      * @return the vehicle registration number
      * @throws IllegalArgumentException if user's input is null value
      */
-    public String readVehicleRegistrationNumber() throws Exception {
+    public String readVehicleRegistrationNumber() {
         try {
             String vehicleRegNumber= scan.nextLine();
             if(vehicleRegNumber == null || vehicleRegNumber.trim().length()==0) {
                 throw new IllegalArgumentException("Invalid input provided");
             }
             return vehicleRegNumber;
-        }catch(Exception e){
+        }catch(IllegalArgumentException e){
             logger.error("Error while reading user input from Shell", e);
             System.out.println("Error reading input. Please enter a valid string for vehicle registration number");
             throw e;
