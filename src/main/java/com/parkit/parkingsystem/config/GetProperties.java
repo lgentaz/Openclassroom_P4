091eced9f6310fcfcee57251232349af6f4a2663
lgentaz <lucile.gentaz@gmail.com>
2021-03-12 09:33:28 +0100

    GetProperties: adding javadoc

--- src/main/java/com/parkit/parkingsystem/config/GetProperties.java
+++ src/main/java/com/parkit/parkingsystem/config/GetProperties.java
@@ -8,6 +8,14 @@ import java.util.*;
 public class GetProperties {
     private static final Logger logger = LogManager.getLogger("GetProperties");
 
+    /**
+     * Retrieves and returns the property value stored in config.properties
+     *
+     * @param key
+     *          string of character corresponding to the property type
+     * @return value
+     *          string of character corresponding to the precific value associated to the key in config.properties file
+     */
 
     public String getProp(String key) {
         Properties prop = new Properties();
@@ -19,7 +27,7 @@ public class GetProperties {
         } catch (IOException ioe) {
             logger.error("Property file '" + propFile + "' not found in the classpath");
         }
-        return prop.getProperty(key);
-
+        String value = prop.getProperty(key);
+        return value;
     }
 }
